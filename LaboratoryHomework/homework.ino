de9e43a099724201b2fd1a20f005defa3e6315fb
alecs99 <47901131+alecs99@users.noreply.github.com>
2019-11-11 14:17:47 +0200

    Update homework.ino

--- LaboratoryHomework/homework.ino
+++ LaboratoryHomework/homework.ino
@@ -1,26 +1,33 @@
 const int speakerPin = A0;
 const int pushButton = 2;
 const int buzzerPin = 11;
+
 int speakerValue = 0;
 int buttonState = 0;
+
 const int threshold = 355;
+
 void setup() {
-pinMode(speakerPin, INPUT);
-Serial.begin(9600);
+  pinMode(speakerPin, INPUT);
+  Serial.begin(9600);
 }
 void loop() {
   speakerValue = analogRead(speakerPin);
+  
   if (speakerValue > 345)
    Serial.println(speakerValue);
+  
   if (speakerValue > threshold) {
      Serial.println("Knock");
-     tone(11, 500);
-     
+     tone(buzzerPin, 500);
   }
+  
   buttonState = digitalRead(pushButton);
+  
   if(buttonState == 1){
-      noTone(11);
+      noTone(buzzerPin);
      }
+  
   delay(10);
 
 }
